e777a794ece4a9591ad6a73415cb620ac4d3e3ce
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2023-01-17 23:21:54 -0800

    Updating with bootstrap prototype
    first push towards #25

---
+++ map/src/js/main.js
@@ -0,0 +1,32 @@
+// Import our custom CSS
+import "../scss/styles.scss";
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+// import bootstrap icons
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+/* global bootstrap: false */
+
+  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  tooltipTriggerList.forEach((tooltipTriggerEl) => {
+    new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+
+
+// when data-bs-toggle is clicked change class to active
+const toggle = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+toggle.forEach((el) => {
+  el.addEventListener("click", () => {
+    // if any other element has class active remove it
+    const active = document.querySelector(".active");
+    if (active) {
+        active.classList.remove("active");
+    }
+    el.classList.toggle("active");
+
+  });
+});
+
+console.log(tooltipTriggerList)
+console.log(toggle)
